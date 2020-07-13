899a277435f773c57a69c3eec39866a5fbe69a61
Siddharth <40488679+siddharth952@users.noreply.github.com>
2020-07-13 11:24:22 +0530

    Update

---
+++ Basics/Recursion/Quick Thinking/countBinaryStrings.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+/*
+ Count Binary Strings if length n which have No consecutive ones 
+*/
+// Wrong this would have been count we need to count possible ways of making abinary string with no 1s together
+// int countNoConOne(string s1){
+
+//     countNoConOne(s1.substr(1,s1.size()-1));
+//     if (s1[0] == 1) return 1;
+//     else return 0;
+
+// }
+
+
+
+int main(){
+    
+   
+}
