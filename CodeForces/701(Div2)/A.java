69cd9d17eeb7a7b19dee5f5a36a1580ff5ca266f
Siddharth <40488679+siddharth952@users.noreply.github.com>
2021-02-18 10:57:33 +0530

    Add answers to CodeForces and Leetcode questions.

---
+++ CodeForces/701(Div2)/A.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class A {
+
+    /*
+
+    */
+    public void print(Object x){
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        // ================= Code here =============
+        FastScanner fs=new FastScanner();
+        int T=fs.nextInt(); // Test Cases
+        for (int tt=0; tt<T; tt++) {
+            int a=fs.nextInt(),b = fs.nextInt();
+            int best = Integer.MAX_VALUE;
+            for (int times=0;times<30;times++){
+                best = Math.min(best, times+count(a,b+times));
+            } print(best);
+        }
+    }
+
+    static int count(int a, int b){
+        if (b==1) return 1_000_000_000;
+        int c = 0;
+        while (a!=0){
+            a /= b;
+            c++;
+        }
+        return c;
+    }
+
+
+
+    //=================== Template =====
+    static class FastScanner {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer("");
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        int[] readArray(int n) {
+            int[] a=new int[n];
+            for (int i=0; i<n; i++) a[i]=nextInt();
+            return a;
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+
+}
--- LeetCode/Other/petrolFilling.cpp
+++ LeetCode/Other/petrolFilling.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int getTour(int petrol[] ,int distance[],int n){
