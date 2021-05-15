c23b00b751cc6f526ff483bd1eda1d8afed47906
Siddharth <40488679+siddharth952@users.noreply.github.com>
2021-05-15 21:13:18 +0530

    Add 702

---
+++ CodeForces/702/B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Question:
+   Solution:
+*/
+
+void solve() {
+    int n,res=0; cin>>n;
+
+    cout << res << endl;
+}
+int main(){
+   ios_base::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t;
+   cin>>t;
+   while(t--){
+       solve();
+   }
+    return 0;
+}
